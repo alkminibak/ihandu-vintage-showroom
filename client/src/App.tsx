d901341499d0286d4da9959a89d739fc21a37b29
alkminibak <alkmini.bak@gmail.com>
2026-08-06 18:58:42 +0300

    fix: refresh wishlist when auth user changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,26 +8,35 @@ import WishlistPage from "./pages/WishlistPage";
 import ProtectedAdminRoute from "./components/ProtectedAdminRoute";
 import AuthProvider from "./contexts/AuthProvider";
 import WishlistProvider from "./contexts/WishlistProvider";
+import { useAuth } from "./hooks/useAuth";
+
+const AppRoutes = () => {
+  const { user } = useAuth();
+
+  return (
+    <WishlistProvider key={user?.id ?? "guest"}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products/:id" element={<ProductDetailsPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/wishlist" element={<WishlistPage />} />
+        <Route
+          path="/admin"
+          element={
+            <ProtectedAdminRoute>
+              <AdminDashboardPage />
+            </ProtectedAdminRoute>
+          }
+        />
+      </Routes>
+    </WishlistProvider>
+  );
+};
 
 const App = () => {
   return (
     <AuthProvider>
-      <WishlistProvider>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/products/:id" element={<ProductDetailsPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/wishlist" element={<WishlistPage />} />
-          <Route
-            path="/admin"
-            element={
-              <ProtectedAdminRoute>
-                <AdminDashboardPage />
-              </ProtectedAdminRoute>
-            }
-          />
-        </Routes>
-      </WishlistProvider>
+      <AppRoutes />
     </AuthProvider>
   );
 };
